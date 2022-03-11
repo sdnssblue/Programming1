4036839506be1efd3212a2e5c151eda17369d07d
Владислав Медяннкин <medyankin03@mail.ru>
2022-03-11 10:00:30 +0700

    EnumValue → EnumValues

--- Programming/View/MainForm.cs
+++ Programming/View/MainForm.cs
@@ -33,38 +33,38 @@ namespace Programming.View
             {
                 ValuesListBox.Items.Clear();
 
-                Array EnumValue;
+                Array EnumValues;
 
                 switch ((Enums)EnumsListBox.SelectedItem)
                 {
                     case Enums.Weekday:
-                        EnumValue = Enum.GetValues(typeof(Weekday));
+                        EnumValues = Enum.GetValues(typeof(Weekday));
                         break;
 
                     case Enums.Genre:
-                        EnumValue = Enum.GetValues(typeof(Genre));
+                        EnumValues = Enum.GetValues(typeof(Genre));
                         break;
 
                     case Enums.Colors:
-                        EnumValue = Enum.GetValues(typeof(Colors));
+                        EnumValues = Enum.GetValues(typeof(Colors));
                         break;
 
                     case Enums.EducationForm:
-                        EnumValue = Enum.GetValues(typeof(EducationForm));
+                        EnumValues = Enum.GetValues(typeof(EducationForm));
                         break;
 
                     case Enums.Smartphones:
-                        EnumValue = Enum.GetValues(typeof(Smartphones));
+                        EnumValues = Enum.GetValues(typeof(Smartphones));
                         break;
 
                     case Enums.Seasons:
-                        EnumValue = Enum.GetValues(typeof(Seasons));
+                        EnumValues = Enum.GetValues(typeof(Seasons));
                         break;
 
                     default:
                         throw new NotImplementedException();
                 }
-                foreach (var value in EnumValue)
+                foreach (var value in EnumValues)
                 {
                     ValuesListBox.Items.Add(value);
                 }
