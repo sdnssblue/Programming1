8a46ba1263e390a318ad67133d2e1fb52cde7d57
Владислав Медяннкин <medyankin03@mail.ru>
2023-03-22 18:20:56 +0700

    feature(MainWindow, Contact): Сверстано главное окно согласно макету, добавлен класс Contact для хранения данных о контактах.

---
+++ src/Contacts/Model/Contact.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    /// <summary>
+    /// Хранит данные о контакте.
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>
+        /// </summary>
+        /// <param name="name">Имя контакта.</param>
+        /// <param name="phone">Номер телефона контакта.</param>
+        /// <param name="email">Адрес электронной почты контакта.</param>
+        public Contact(string name, string phone, string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Создает экземпляр класса <see cref="Contact"/>
+        /// </summary>
+        public Contact()
+        {
+
+        }
+
+        /// <summary>
+        /// Возвращает и задает имя контакта.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает номер телефона контакта.
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает адрес электронной почты контакта.
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
