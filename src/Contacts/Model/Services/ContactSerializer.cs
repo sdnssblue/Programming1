344b16ad451967a89e0ce43782b6d6d342136d45
Владислав Медяннкин <medyankin03@mail.ru>
2023-03-29 13:36:01 +0700

    feature(ContactSerializer, MainVM, MainWindow): добавлена сериализация контакта, добавлены команды для сохранения и загрузки контакта, выполнена привязка событий на главном окне

---
+++ src/Contacts/Model/Services/ContactSerializer.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace View.Model.Services
+{
+    public static class ContactSerializer
+    {
+        /// <summary>
+        /// Сериализацию данных.
+        /// </summary>
+        /// <param name="contact">Контакт.</param>
+        /// <param name="path">Путь сериализации.</param>
+        public static void Serialize(Contact contact, string path)
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(path)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+            }
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.Write(JsonConvert.SerializeObject(contact));
+            }
+        }
+
+        /// <summary>
+        /// Десериализация данных.
+        /// </summary>
+        /// <param name="path">Путь.</param>
+        /// <returns>Возвращает экземпляр класса <see cref="Contact"/>.</returns>
+        public static Contact Deserialize(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+            }
+
+            var contact = new Contact();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    contact = JsonConvert.DeserializeObject<Contact>(reader.ReadToEnd());
+                }
+
+                if (contact == null) contact = new Contact();
+            }
+            catch (FileNotFoundException e)
+            {
+                return contact;
+            }
+
+            return contact;
+        }
+    }
+}
--- src/Contacts/ViewModel/MainVM.cs
+++ src/Contacts/ViewModel/MainVM.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using View.Model;
+using View.Model.Services;
 
 namespace View.ViewModel
 {
@@ -11,6 +15,13 @@ namespace View.ViewModel
         public Contact Contact { get; private set; } = new Contact();
 
         /// <summary>
+        /// Возвращает и задаёт путь сериализации. По умолчанию - папка "Мои документы".
+        /// </summary>
+        public string Path { get; set; } =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            + @"\Contacts\contacts.json";
+
+        /// <summary>
         /// Возвращает и задает имя контакта.
         /// </summary>
         public string Name
@@ -19,6 +30,7 @@ namespace View.ViewModel
             set
             {
                 Contact.Name = value;
+                OnPropertyChanged(nameof(Name));
             }
         }
 
@@ -31,6 +43,7 @@ namespace View.ViewModel
             set
             {
                 Contact.PhoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
             }
         }
 
@@ -43,7 +56,41 @@ namespace View.ViewModel
             set
             {
                 Contact.Email = value;
+                OnPropertyChanged(nameof(Email));
             }
+        }
+
+        /// <summary>
+        /// Команда сериализации контакта.
+        /// </summary>
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return new RelayCommand((obj) =>
+                {
+                    ContactSerializer.Serialize(Contact, Path);
+                });
+            }
+        }
+
+        /// <summary>
+        /// Команда десериализации контакта.
+        /// </summary>
+        public ICommand LoadCommand
+        {
+            get => new RelayCommand((obj) =>
+            {
+                var contact = ContactSerializer.Deserialize(Path);
+                Name = contact.Name;
+                Email = contact.Email;
+                PhoneNumber = contact.PhoneNumber;
+            });
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
         }
 
         /// <summary>
